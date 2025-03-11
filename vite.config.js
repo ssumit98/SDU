c14b041ad5dfaf9bfda9fe8bcf2d6e8890947b40
bisonsports <bisonsports@gmail.com>
2025-03-12 01:57:01 +0530

    url fix

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/SDU/', // Replace with your GitHub repository name
+  base: 'SDU/', // Replace with your GitHub repository name
 });
